fe7dbb32825085f1c75d612e95aa79d26e3e8b99
PHunt14 <PHunt14@users.noreply.github.com>
2016-04-08 14:33:52 -0400

    Create login-form.php

---
+++ login-form.php
@@ -0,0 +1,21 @@
+<html>
+<head><title> Login Form | Simple Login & Logout Using MySQL Database And PHP Script </title></head>
+<body>
+<form action="login-script.php" method="post">
+<table>
+<tr>
+<td>Username</td>
+<td><input type="text" name="username" size="25"></td>
+</tr>
+<tr>
+<td>Password</td>
+<td><input type="password" name="password" size="30"></td>
+</tr>
+<tr>
+<td>&nbsp;</td>
+<td><input type="submit" name="Login" value="Proses"></td>
+</tr>
+</table>
+</form>
+</body>
+</html>
