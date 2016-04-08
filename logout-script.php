a781509c22a849a2d0c003bb4b964c532c7f484a
PHunt14 <PHunt14@users.noreply.github.com>
2016-04-08 14:36:47 -0400

    Create logout-script.php

---
+++ logout-script.php
@@ -0,0 +1,6 @@
+<?php session_start();
+session_destroy(); 
+echo "You have successfully logged out!";
+echo "<br/>Are you want to login ? Click here : <a href='login-form.php'>Login Form</a>";
+echo "<br/><a href='http://tutorial.world.edu/website-programming/how-to-create-login-logout-page-using-mysql-database-php-script/'>Tutorial</a>";
+?>
