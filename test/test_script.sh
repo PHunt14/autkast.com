f6519e5d5b5c3eabf2806f8e6037037223b80776
PHunt14 <PHunt14@users.noreply.github.com>
2022-05-05 14:01:50 -0400

    testing drone scripts

--- test/test_script.sh
+++ test/test_script.sh
@@ -6,8 +6,40 @@ someFunction() {
     endTime=$(( $(date +%s) + userTimeout ))
     for component in foo bar
     do
-        echo "checking $component"
+        echo "checking $component in $environment"
     done
 }
 
+# gitDiff() {
+#     changedFile=$(git diff --name-only index.html fanews.css)
+#     if [ "$changedFile" = "index.html"
+# }
+
+help() {
+	# display help
+	echo "Run a test script with some fake options"
+	echo
+	echo "Syntax ./test_script.sh [-h] environment [dev|test] timeout (seconds)"
+	echo "environment has two options, 'dev' or 'test'"
+	echo "options:"
+	echo "h		print this help"
+	echo
+}
+
+while getopts ":h" option; do
+	case $option in
+		h) #display help
+			help
+			exit;;
+		# t) # change the timeout
+		# 	userTimeout=$OPTARG;;
+		# 	setTimeout($userTimeout)
+		\?) #invalid option
+			echo "Error: Invalid option"
+		 	exit;;
+	esac
+done
+
+environment=$1
+userTimeout=$2
 someFunction
