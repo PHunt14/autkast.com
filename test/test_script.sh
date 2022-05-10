6944d46883ee216a844c1cde541acd9332f5e991
PHunt14 <PHunt14@users.noreply.github.com>
2022-05-10 14:59:49 -0400

    Update test_script.sh

--- test/test_script.sh
+++ test/test_script.sh
@@ -8,7 +8,6 @@ checkEnvironment(){
 # check if specific files have been updated.  to add to this list simply update the for loop with a new value.
 	for environment in qa uat
 	do
-		echo $environment
 		# changedFile=$(git diff $DRONE_BRANCH origin/master --name-only test-$environment.yaml)
 		changedFile=$(git diff HEAD^ --name-only test-$environment.yaml)
 		if [ -n "$changedFile" ]; then
